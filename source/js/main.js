11a6bf0f78e49d7db1f975c66233f5df9aa5a6b8
Ulia Usalko <usalko.ulia@gmail.com>
2018-04-11 22:42:16 +0300

    Починила ошибку формы

--- source/js/main.js
+++ source/js/main.js
@@ -2,7 +2,6 @@ var header = document.querySelector(".header");
 var header_toggle = document.querySelector(".header__menu-toggle");
 
 var search_form = document.querySelector(".programs__form");
-var submit_button = document.querySelector(".programs__button");
 var form_name = search_form.querySelector("#form-name");
 var form_weight = search_form.querySelector("#form-weight");
 var form_email = search_form.querySelector("#form-email");
@@ -20,25 +19,28 @@ header_toggle.addEventListener("click", function(evt) {
 });
 
 search_form.addEventListener("submit", function(evt) {
-  evt.preventDefault();
 
-  if (!form_name.value) {
-    form_name.classList.add("picking-form__text-input--error");
-    form_name.focus();
+  if (!form_phone.value) {
+    evt.preventDefault();
+    form_phone.classList.add("picking-form__text-input--error");
+    form_phone.focus();
   }
 
-  if (!form_name.value) {
+  if (!form_email.value) {
+    evt.preventDefault();
+    form_email.classList.add("picking-form__text-input--error");
+    form_email.focus();
+  }
+
+  if (!form_weight.value) {
+    evt.preventDefault();
     form_weight.classList.add("picking-form__text-input--error");
     form_weight.focus();
   }
 
   if (!form_name.value) {
-    form_email.classList.add("picking-form__text-input--error");
-    form_email.focus();
-  }
-
-  if (!form_name.value) {
-    form_phone.classList.add("picking-form__text-input--error");
-    form_phone.focus();
+    evt.preventDefault();
+    form_name.classList.add("picking-form__text-input--error");
+    form_name.focus();
   }
 });
