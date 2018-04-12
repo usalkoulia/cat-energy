d302a0644a6c79c57c26810d733fdbc7f80113bd
Ulia Usalko <usalko.ulia@gmail.com>
2018-04-12 13:02:58 +0300

    Исправила баги сабмита формы

--- source/js/main.js
+++ source/js/main.js
@@ -1,12 +1,6 @@
 var header = document.querySelector(".header");
 var header_toggle = document.querySelector(".header__menu-toggle");
 
-var search_form = document.querySelector(".programs__form");
-var form_name = search_form.querySelector("#form-name");
-var form_weight = search_form.querySelector("#form-weight");
-var form_email = search_form.querySelector("#form-email");
-var form_phone = search_form.querySelector("#form-phone");
-
 header.classList.remove("header--active");
 
 header_toggle.addEventListener("click", function(evt) {
@@ -18,29 +12,38 @@ header_toggle.addEventListener("click", function(evt) {
   }
 });
 
-search_form.addEventListener("submit", function(evt) {
+var search_form = document.querySelector(".programs__form");
 
-  if (!form_phone.value) {
-    evt.preventDefault();
-    form_phone.classList.add("picking-form__text-input--error");
-    form_phone.focus();
-  }
+if (search_form) {
+  var form_name = search_form.querySelector("#form-name");
+  var form_weight = search_form.querySelector("#form-weight");
+  var form_email = search_form.querySelector("#form-email");
+  var form_phone = search_form.querySelector("#form-phone");
 
-  if (!form_email.value) {
-    evt.preventDefault();
-    form_email.classList.add("picking-form__text-input--error");
-    form_email.focus();
-  }
+  search_form.addEventListener("submit", function(evt) {
 
-  if (!form_weight.value) {
-    evt.preventDefault();
-    form_weight.classList.add("picking-form__text-input--error");
-    form_weight.focus();
-  }
+    if (!form_phone.value) {
+      evt.preventDefault();
+      form_phone.classList.add("picking-form__text-input--error");
+      form_phone.focus();
+    }
 
-  if (!form_name.value) {
-    evt.preventDefault();
-    form_name.classList.add("picking-form__text-input--error");
-    form_name.focus();
-  }
-});
+    if (!form_email.value) {
+      evt.preventDefault();
+      form_email.classList.add("picking-form__text-input--error");
+      form_email.focus();
+    }
+
+    if (!form_weight.value) {
+      evt.preventDefault();
+      form_weight.classList.add("picking-form__text-input--error");
+      form_weight.focus();
+    }
+
+    if (!form_name.value) {
+      evt.preventDefault();
+      form_name.classList.add("picking-form__text-input--error");
+      form_name.focus();
+    }
+  });
+}
